acdfde36ff29752c49543803016004f383c66f7f
Steve <lorenz21@hawaii.edu>
2018-12-15 09:34:11 -1000

    updated static build files for heroku client side

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ function normalizePort(val) {
 }
 
 if(process.env.NODE_ENV === 'production'){
-	app.use(express.static('clien/build'));
+	app.use(express.static('client/build'));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
