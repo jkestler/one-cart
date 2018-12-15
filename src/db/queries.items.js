1921b9423f3f9dcaf5001f22a4003d8f37aa0256
Steve <lorenz21@hawaii.edu>
2018-12-15 10:18:32 -1000

    updated sequelize config

--- src/db/queries.items.js
+++ src/db/queries.items.js
@@ -11,7 +11,7 @@ module.exports = {
       });
    },
    getItem(id, callback){
-      return Item.findByPk(id)
+      return Item.findById(id)
       .then((item) => {
          callback(null, item);
       })
@@ -31,7 +31,7 @@ module.exports = {
       })
     },
    deleteItem(req, callback) {
-      return Item.findByPk(req.params.id)
+      return Item.findById(req.params.id)
       .then((item) => {
             item.destroy()
             .then((res) => {
@@ -43,7 +43,7 @@ module.exports = {
       });
    },
    updateItem(id, updatedItem, callback) {
-      return Item.findByPk(id)
+      return Item.findById(id)
       .then((item) => {
          if(!item) {
             return callback("Item not found")
@@ -60,7 +60,7 @@ module.exports = {
       })
    },
    setItemPurchase(id, purchaseStatus, callback) {
-      return Item.findByPk(id)
+      return Item.findById(id)
       .then((item) => {
          if(!item) {
             return callback("Item not found")
